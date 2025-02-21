4fa94ff953b508db2427770a8a3ebdfc25ec22d6
Daniel H <danielmople1@gmail.com>
2025-02-20 20:35:45 -0500

    Changed up look of Coin, added Jump control, changed map layout

---
+++ Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+
+// Include the namespace required to use Unity UI
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	// Create public variables for player speed, and for the Text UI game objects
+	public float speed;
+	public float jumpForce = 5.0f;
+	public float gravityMultiplier = 2.5f;
+	public Text countText;
+	public Text winText;
+
+	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+	private Rigidbody rb;
+	private int count;
+	private bool isGrounded;
+
+	// At the start of the game..
+	void Start ()
+	{
+		// Assign the Rigidbody component to our private rb variable
+		rb = GetComponent<Rigidbody>();
+
+		// Set the count to zero 
+		count = 0;
+
+		// Run the SetCountText function to update the UI (see below)
+		SetCountText ();
+
+		// Set the text property of our Win Text UI to an empty string, making the 'You Win' (game over message) blank
+		winText.text = "";
+	}
+    void Update()
+    {
+		
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
+		if (isGrounded && Input.GetKeyDown(KeyCode.Space)) // You can change the input key here
+        {
+            Jump();
+        }
+    }
+
+    // Each physics step..
+    void FixedUpdate ()
+	{
+		/*// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		// Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
+		// multiplying it by 'speed' - our public player speed that appears in the inspector
+		rb.AddForce (movement * speed);
+		*/
+		MovePlayer();
+		rb.AddForce(Physics.gravity * gravityMultiplier, ForceMode.Acceleration);
+	}
+
+	void MovePlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+        rb.AddForce(movement * speed);
+    }
+
+	// When this game object intersects a collider with 'is trigger' checked, 
+	// store a reference to that collider in a variable named 'other'..
+	void OnTriggerEnter(Collider other) 
+	{
+		// ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			// Make the other game object (the pick up) inactive, to make it disappear
+			other.gameObject.SetActive (false);
+
+			// Add one to the score variable 'count'
+			count = count + 1;
+
+			// Run the 'SetCountText()' function (see below)
+			SetCountText ();
+		}
+	}
+	void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+	
+	void SetCountText()
+	{
+		// Update the text field of our 'countText' variable
+		countText.text = "Coins Collected: " + count.ToString() +"/12";
+
+		// Check if our 'count' is equal to or exceeded 12
+		if (count >= 12) 
+		{
+			// Set the text value of our 'winText'
+			winText.text = "You Win!";
+		}
+	}
+}
